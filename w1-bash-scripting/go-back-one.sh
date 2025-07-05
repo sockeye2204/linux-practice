cab5b11749aaa7c60b39ac554409a67f40c625ee
sockeye2204 <sockeye.2204@gmail.com>
2025-07-05 16:46:09 +0100

    Delete w1-bash-scripting directory

--- w1-bash-scripting/go-back-one.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script just... goes back one! By that, I mean we go down the directory tree by one level.
-WD="$(pwd)"
-
-if [ $WD = "/" ]; then
-    echo "At the root directory!"
-else
-    cd ../
-fi
--- w1-bash-scripting/hello-world.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# The above is the shebang, and it informs the OS of the path of the interpreter.
-echo "Hello, world! - 1"
-# This prints Hello, world! - 1 to the terminal.
-echo "Hello..."; echo "world! - 2"
-# As you can see, we can chain same-line commands with a semicolon.
-name="earth"
-echo "Hello, $name!"
-# Look, this time we assigned a value to a variable and used that variable in echo's argument.
